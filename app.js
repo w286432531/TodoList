7aed8b43d9f18da1a5e5048f431b8aeb928125a6
Jay Huang <huang.jianzhu@gmail.com>
2020-10-09 21:44:18 -0400

    set up port

--- app.js
+++ app.js
@@ -167,8 +167,5 @@ app.get("/:pageName",(req,res)=>{
   });
 
 });
-let port = process.env.PORT;
-if (port == null || port = "") {
-  port = 3000;
-}
-app.listen(port,()=> console.log("Server started"));
+
+app.listen(process.env.PORT || 3000,()=> console.log("Server started"));
